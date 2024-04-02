873b04b581ed08a1779c9881e00d7576cc924157
Nurul Anam <51789412+nurulanam@users.noreply.github.com>
2024-04-02 06:50:30 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =144553
+let accountEmail = "nurulanam.bd@gmail.com"
+var accountPass = "12345"
+accountCity = "Satkhira"
+let accountState;
+
+// accountId = 2
+accountEmail = "test@mail.com"
+accountPass = "asdf"
+accountCity = "Khulna"
+
+// console.log(accountEmail);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState])
